96607c6c8b1edeeab5ae8ec95c5869fd4b4381c7
= <mpenaloza@uc.cl>
2023-08-21 10:55:13 -0400

    feat: Puppeteer optimized

--- scrapper/app.js
+++ scrapper/app.js
@@ -3,6 +3,8 @@ import { startBrowser, stopBrowser, getProductFromJumbo } from './jumbo.js';
 const app = express();
 const port = 5000;
 
+app.use(express.json());
+
 const {browser, page} = await startBrowser();
 
 app.get('/', (req, res) => {
@@ -12,7 +14,7 @@ app.get('/', (req, res) => {
 app.get('/product/:productCode', async (req, res) => {
   const productCode = req.params.productCode;
   const productInfo = await getProductFromJumbo(productCode, browser, page);
-  res.send(productInfo);
+  res.json(productInfo);
 })
 
 app.listen(port, () => {
--- scrapper/jumbo.js
+++ scrapper/jumbo.js
@@ -2,16 +2,58 @@ import puppeteer from "puppeteer";
 let code = "7801620852955";
 let code2 = "7802910083509";
 
+
+const minimal_args = [
+  '--autoplay-policy=user-gesture-required',
+  '--disable-background-networking',
+  '--disable-background-timer-throttling',
+  '--disable-backgrounding-occluded-windows',
+  '--disable-breakpad',
+  '--disable-client-side-phishing-detection',
+  '--disable-component-update',
+  '--disable-default-apps',
+  '--disable-dev-shm-usage',
+  '--disable-domain-reliability',
+  '--disable-extensions',
+  '--disable-features=AudioServiceOutOfProcess',
+  '--disable-hang-monitor',
+  '--disable-ipc-flooding-protection',
+  '--disable-notifications',
+  '--disable-offer-store-unmasked-wallet-cards',
+  '--disable-popup-blocking',
+  '--disable-print-preview',
+  '--disable-prompt-on-repost',
+  '--disable-renderer-backgrounding',
+  '--disable-setuid-sandbox',
+  '--disable-speech-api',
+  '--disable-sync',
+  '--hide-scrollbars',
+  '--ignore-gpu-blacklist',
+  '--metrics-recording-only',
+  '--mute-audio',
+  '--no-default-browser-check',
+  '--no-first-run',
+  '--no-pings',
+  '--no-sandbox',
+  '--no-zygote',
+  '--password-store=basic',
+  '--use-gl=swiftshader',
+  '--use-mock-keychain',
+]
+
 export async function startBrowser() {
   console.log("Iniciando browser...")
   const browser = await puppeteer.launch({
     headless: "new",
     defaultViewport: null,
-    args: [
-      '--no-sandbox',
-    ],
+    args: minimal_args,
   });
   const page = await browser.newPage();
+  await page.setRequestInterception(true)
+  page.on('request', (request) => {
+    if (request.resourceType() === 'image' || request.resourceType() === 'font') request.abort()
+    else request.continue()
+  })
   await page.goto("https://www.jumbo.cl/", {
     waitUntil: 'domcontentloaded'
   });
