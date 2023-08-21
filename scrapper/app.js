99c01c28980f25e300f7afbe6a4ef492dfb212be
= <mpenaloza@uc.cl>
2023-08-20 23:32:14 -0400

    feat: app working locally but not on docker

--- scrapper/app.js
+++ scrapper/app.js
@@ -1,10 +1,19 @@
-const express = require('express');
+import express from 'express';
+import { startBrowser, stopBrowser, getProductFromJumbo } from './jumbo.js';
 const app = express();
 const port = 5000;
+
+const {browser, page} = await startBrowser();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.get('/product/:productCode', async (req, res) => {
+  const productCode = req.params.productCode;
+  const productInfo = await getProductFromJumbo(productCode, browser, page);
+  res.send(productInfo);
+})
 
 app.listen(port, () => {
   console.log(`Scrapper running on port ${port}`)
