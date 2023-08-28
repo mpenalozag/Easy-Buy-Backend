b0121b7ae3b8d96a6a1fd00263dca4f0b7a3ef77
= <mpenaloza@uc.cl>
2023-08-27 22:59:15 -0400

    feat: Added auth controllers

---
+++ api/controllers/auth/login.js
@@ -0,0 +1,29 @@
+const bcrypt = require("bcrypt");
+const { User } = require("../../models");
+const jwt = require("jsonwebtoken");
+
+module.exports = async function login() {
+  const {email, password} = req.body;
+  try {
+    const user = await User.findOne({ where:{email: email}});
+    if (!user) {
+      return res.status(404).send({ message: "Usuario no encontrado." });
+    }
+    const passwordIsValid = bcrypt.compare(password, user.password);
+    if (!passwordIsValid) {
+      return res.status(401).send({
+        accessToken: null,
+        message: "Contraseña inválida!"
+      });
+    }
+    const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, {
+      algorithm: "HS256",
+      expiresIn: 1 * 24 * 60 * 60 * 1000
+    });
+    res.status(200).json({
+      accessToken: token,
+    })
+  } catch (err) {
+    console.log(err);
+  }
+}
---
+++ api/controllers/auth/register.js
@@ -0,0 +1,26 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { User } = require("../../models");
+
+module.exports = async function signUp() {
+  try {
+    const { email, password } = req.body;
+    const data = {
+      userName,
+      email,
+      password: bcrypt.hash(password, 10)
+    };
+    const user = await User.create(data);
+    if (user) {
+      let token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, {
+        algorithm: "HS256",
+        expiresIn: 1 * 24 * 60 * 60 * 1000
+      });
+      res.status(200).json({
+        accessToken: token,
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
