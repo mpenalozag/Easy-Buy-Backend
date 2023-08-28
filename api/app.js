363d922ffa171e4273778fe4961017a029f0512c
= <mpenaloza@uc.cl>
2023-08-27 21:50:21 -0400

    feat: 'User' model added to database

--- api/app.js
+++ api/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.API_PORT;
+
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
--- api/start.sh
+++ api/start.sh
@@ -1,4 +1,6 @@
 echo "Setting up the database..."
 npx sequelize-cli db:create
-echo "Database created!"
+echo "Migrating the database..."
+npx sequelize-cli db:migrate
+echo "Database ready!"
 node app.js
